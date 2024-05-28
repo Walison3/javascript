c553f04393d0a117cfd5a2a8e2483f28a95f5031
Walison3 <147935411+Walison3@users.noreply.github.com>
2024-05-27 22:09:47 -0300

    Exercícios

---
+++ Exercícios/ex008/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocide do seu carro é de ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você passou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
